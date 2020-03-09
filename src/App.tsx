921d22b30eeefe5f78a6f0836c06fdf4deb18a1c
Ben Awad <benawad97@gmail.com>
2020-03-09 12:50:18 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useState } from "react";
+import { createEditor, Node, Editor, Transforms } from "slate";
+import { Editable, Slate, withReact, ReactEditor } from "slate-react";
+import "./App.css";
+
+const withWorkingPaste = (editor: Editor & ReactEditor) => {
+  const { insertData } = editor;
+
+  editor.insertData = data => {
+    const text = data.getData("text/plain");
+    if (text) {
+      let i = 0;
+      for (const line of text.split(/\r\n|\r|\n/)) {
+        if (!i) {
+          Transforms.insertText(editor, line);
+        } else {
+          Transforms.insertNodes(editor, {
+            type: "paragraph",
+            children: [{ text: line }]
+          });
+        }
+        i++;
+      }
+      return;
+    }
+
+    insertData(data);
+  };
+
+  return editor;
+};
 
 function App() {
+  const editor = useMemo(() => withWorkingPaste(withReact(createEditor())), []);
+  const [value, setValue] = useState<Node[]>([
+    {
+      type: "paragraph",
+      children: [{ text: "I am sentence number one." }]
+    },
+    {
+      type: "paragraph",
+      children: [{ text: "And I am sentence number 2." }]
+    }
+  ]);
+  // Render the Slate context.
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Slate
+          editor={editor}
+          value={value}
+          onChange={value => setValue(value)}
         >
-          Learn React
-        </a>
+          <Editable
+            style={{
+              backgroundColor: "#fafafa",
+              color: "#444",
+              padding: 20,
+              minHeight: 200,
+              minWidth: 400
+            }}
+          />
+        </Slate>
       </header>
     </div>
   );
